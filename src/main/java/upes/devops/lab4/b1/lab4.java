99f47a9a4fc21024130f9281143a12a8e9349a9d
Antriksh <antrikshbahri@gmail.com>
2023-02-19 11:19:19 +0530

    Second commit

--- src/main/java/upes/devops/lab4/b1/lab4.java
+++ src/main/java/upes/devops/lab4/b1/lab4.java
@@ -2,10 +2,11 @@ package upes.devops.lab4.b1;
 
 public class lab4 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a=5,b=9;
 		
 		System.out.println(a+b);
+		System.out.println(a-b);
 
 	}
 
