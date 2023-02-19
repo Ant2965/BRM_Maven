5b91d7b1ed79238b657c3bd8e8d01f10740c03ad
Antriksh <antrikshbahri@gmail.com>
2023-02-19 09:24:19 +0530

    first commit

---
+++ src/main/java/upes/devops/lab4/b1/lab4.java
@@ -0,0 +1,13 @@
+package upes.devops.lab4.b1;
+
+public class lab4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=9;
+		
+		System.out.println(a+b);
+
+	}
+
+}
+
